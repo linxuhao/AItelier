5c1362f4ec7094c332312ae7dac47d3727a17ae9
AItelier <aitelier@localhost>
2026-07-07 11:24:09 +0000

    dpe_default_v2/t_impl: redirect_to_dashboard [aitelier-repo-view-2] (1 file(s))

---
+++ web/src/views/RedirectToDashboard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { push } from 'svelte-spa-router';
+
+  onMount(() => {
+    push('#/');
+  });
+</script>
+
+<article aria-busy="true">
+  <p>Redirecting…</p>
+</article>
